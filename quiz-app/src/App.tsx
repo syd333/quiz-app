10be76f881ed6a028335e225f5479e4f44ea2ee0
syd333 <sydmiranda@icloud.com>
2021-06-29 18:38:24 -0400

    added types for answers

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -5,13 +5,20 @@ import QuestionCard from "./components/QuestionCard";
 // types
 import { QuestionState, Difficulty } from "./API";
 
+type AnswerObject = {
+      question: string;
+      answer: string;
+      correct: boolean;
+      correctAnswer: string;
+}
+
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
