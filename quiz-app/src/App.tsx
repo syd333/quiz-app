cdeeb37bf07abb26f4f7becd6bd1dc21fe974495
syd333 <sydmiranda@icloud.com>
2021-06-29 18:36:14 -0400

    fetched data from API and made shufflearray function

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import { fetchQuizQuestions } from "./API";
+//components
 import QuestionCard from "./components/QuestionCard";
+// types
+import { QuestionState, Difficulty } from "./API";
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -16,7 +31,14 @@ const App = () => {
       </button>
       <p className="score">Score</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      {/* <QuestionCard 
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
