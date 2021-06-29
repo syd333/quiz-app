2e03b0576975f379751c0b10cd1e6e993a3230a0
syd333 <sydmiranda@icloud.com>
2021-06-29 16:53:57 -0400

    App.tsx file

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
+const App = () => {
+  const startTrivia = async () => {};
 
-function App() {
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="App">
-     quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
